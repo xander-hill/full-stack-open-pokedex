a1f2790bb7c40fc51a8c80a99b6796be9fda87c8
Xander Hill <xander.k.hill@gmail.com>
2025-01-14 22:09:17 -0600

    stuff for lint fixed

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const app = express();
+import express, { static as stattic } from 'express'
+const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.use(express.static("dist"));
+app.use(stattic('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+  //eslint-disable-next-line
+  console.log(`server started on port ${PORT}`)
+})
--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 
+//eslint-disable-next-line
 ReactDOM.createRoot(document.getElementById('app')).render(
   <Router>
     <App />
